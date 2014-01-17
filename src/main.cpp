7547396f4348eb97152368249134afbc2e7df129
Alexandre Boucey <alexandre.boucey@gmail.com>
2014-01-17 19:54:40 +0100

    New command line options

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,7 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <cstring>
 
 #include "SudokuSolver.h"
 #include "BasicSudokuSolver.h"
@@ -55,13 +56,47 @@ void testWithSolver(SudokuSolver *solver, string grille)
     solver->affiche();
 }
 
+void displayHelp(char cmdline[])
+{
+    cout << "Usage: " << cmdline << endl;
+    cout << "--grid=<file>" << '\t' << "Select a grid file" << endl;
+    cout << "--skip_basic" << '\t' << "Skip BasicSudokuSolver (because it's slow !)" << endl;
+    cout << endl << "Without --grid=<file>, the program will try to load default.txt in the current working directory" << endl;
+}
+
 
 int main(int argc, char *argv[])
 {
+    cout << "SudokuSolver by Alexandre Boucey" << endl;
+    cout << "http://pro.areku.fr/" << endl;
+    cout << "Use --help to see options" << endl << endl;
 
-    string grille("");
-    if(argc == 2)
-        grille = argv[1];
+
+    // lecture de la ligne de commande
+    bool skip_basic = false;
+    string grille("default.txt");
+
+    string skip_basic_cmd("--skip_basic");
+    string grid_cmd("--grid=");
+    string help_cmd("--help");
+
+    for (int i = 0; i < argc; ++i)
+    {
+        if(skip_basic_cmd.compare(argv[i]) == 0)
+        {
+            skip_basic = true;
+        }
+        else if(help_cmd.compare(argv[i]) == 0)
+        {
+            displayHelp(argv[0]);
+            return 0;
+        }
+        else if(grid_cmd.compare(0, 7, argv[i], 0, 7) == 0)
+        {
+            grille = argv[i];
+            grille = grille.substr(7);
+        }
+    }
 
     cout << "Fichier grille: " << grille << endl;
 
@@ -75,8 +110,15 @@ int main(int argc, char *argv[])
          << endl << "==============================" << endl << endl;
     testWithSolver(&asolver, grille);
 
-    BasicSudokuSolver bsolver;
-    cout << endl << "Test avec BasicSudokuSolver"
-         << endl << "==============================" << endl << endl;
-    testWithSolver(&bsolver, grille);
+    if(!skip_basic)
+    {
+        BasicSudokuSolver bsolver;
+        cout << endl << "Test avec BasicSudokuSolver"
+             << endl << "==============================" << endl << endl;
+        testWithSolver(&bsolver, grille);
+    }
+    else
+    {
+        cout << endl << "Skipped BasicSudokuSolver" << endl;
+    }
 }
